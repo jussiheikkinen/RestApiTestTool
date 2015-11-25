daca4c22e34fd608fe3e33537e0b853a054c4d5d
njokipal <H3136@student.jamk.fi>
2015-11-25 18:01:24 +0200

    aspi

---
+++ ASP/About.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="About API" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="ASP.About" %>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>REST API Testing Tool for Tuulari data</h3>
+    <section>
+        <h4>About API</h4>
+        <p>The API is used for our Android app. It uses basic HTTP methods (POST, GET, PUT and DELETE) to create, read, update and delete (CRUD) data from MSSQL database.</p>
+        <h4>Endpoints</h4>
+        <dl>
+            <dt><a href="/api/_">/api/_</a></dt><dd>_</dd>
+        </dl>
+    </section>
+    <section>
+        <h4>This tool</h4>
+        <p>This tool is a ASP.NET version of our API testing tool. There is also a WPF version.</p>
+    </section>
+</asp:Content>
---
+++ ASP/Tool.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addKeyValuePair_Click(object sender, EventArgs e)
+        {
+            TableRow row = new TableRow();
+            TableCell keyCell = new TableCell(), valueCell = new TableCell();
+            TextBox key = new TextBox(), value = new TextBox();
+
+            key.ID = "key[]";
+            value.ID = "value[]";
+
+            keyCell.Controls.Add(key);
+            valueCell.Controls.Add(value);
+
+            row.Cells.Add(keyCell);
+            row.Cells.Add(valueCell);
+
+            keyValuePairTable.Rows.Add(row);
+        }
+    }
+}
